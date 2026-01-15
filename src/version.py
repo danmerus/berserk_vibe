4c2fd03e7a9495c3b3a0065328f24bd6a536730a
unknown <melkozerov414@gmail.com>
2026-01-15 18:49:25 +0100

    Bump version to 0.3.3

--- src/version.py
+++ src/version.py
@@ -1,6 +1,6 @@
 """Version info and update configuration."""
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 # GitHub repository for updates
 GITHUB_OWNER = "danmerus"  # Change this to your GitHub username
